fb04ef2bdaecf43486332745bd72cc80ff8284fb
Borja Fernandez Pillado <borja.fpillado@gmail.com>
2013-11-05 23:15:15 +0100

    Added new requests of games and plays

--- rest-api.js
+++ rest-api.js
@@ -28,3 +28,33 @@ server.del('/user/:id', function (req, res, next) {
 	console.log('Someone wants to delete %s', req.params.id)
 	res.send()
 })
+
+server.get('/game/:id', function (req, res, next) {
+	console.log('Someone asks for the game %s', req.params.id)
+	res.send()
+})
+
+server.post('/game', function (req, res, next) {
+	console.log('Someone wants to create a new game called %s', req.params.name)
+	res.send(201)
+})
+
+server.put('/game/:id', function (req, res, next) {
+	console.log('Someone wants to update the game %s', req.params.name)
+	res.send()
+})
+
+server.del('/game/:id', function (req, res, next) {
+	console.log('Someone wants to delete the game %s', req.params.id)
+	res.send()
+})
+
+server.get('/play/:id', function (req, res, next) {
+	console.log('Someone asks for the play %s', req.params.id)
+	res.send()
+})
+
+server.post('/play', function (req, res, next) {
+	console.log('Someone wants to create a new play called %s', req.params.name)
+	res.send(201)
+})
