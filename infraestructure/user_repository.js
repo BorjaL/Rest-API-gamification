8f5414670d376407b5ba4d748adda3681a8ebcc2
Borja Fernandez Pillado <borja.fpillado@gmail.com>
2013-11-06 20:47:48 +0100

    Adding save method for users

--- infraestructure/user_repository.js
+++ infraestructure/user_repository.js
@@ -7,4 +7,30 @@ UserRepository = function(host, port) {
   this.db.open(function(){})
 }
 
+UserRepository.prototype.getCollection= function(callback) {
+	this.db.collection('players', function(error, players_collection) {
+    if( error ) callback(error)
+    else callback(null, players_collection)
+  });
+};
+
+UserRepository.prototype.save = function(players, callback) {
+    this.getCollection(function(error, players_collection) {
+      if( error ) callback(error)
+      else {
+        if( typeof(players.length) === "undefined" )
+          players = [players];
+
+        for( var i =0;i< players.length;i++ ) {
+          player = players[i];
+          player.created_at = new Date();
+        }
+
+        players_collection.insert(players, function() {
+          callback(null, players);
+        });
+      }
+    });
+};
+
 exports.UserRepository = UserRepository
