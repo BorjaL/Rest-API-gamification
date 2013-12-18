91a639e337e8bdc2ee29b97723b604569ee38386
Borja Fernandez Pillado <borja.fpillado@gmail.com>
2013-12-19 00:08:29 +0100

    Avoiding prototype use

--- infraestructure/player_repository.js
+++ infraestructure/player_repository.js
@@ -3,18 +3,24 @@ var mongojs = require('mongojs')
 PlayerRepository = function() {
 	var config = require('../config/enviroments').setUp()
 	this.db = mongojs(config.mongodb.url+":"+config.mongodb.port+"/"+config.mongodb.name, ['players'])
-}
 
-PlayerRepository.prototype.save = function(player, callback) {
-	this.db.players.save(player, function(error, result) {
-		if ( error ) callback(error)
-		callback(null, result)
-	})
-}
+	this.save = function(player, callback) {
+		this.db.players.save(player, function(error, result) {
+			if ( error ) callback(error)
+			callback(null, result)
+		})
+	}
 
-PlayerRepository.prototype.clean = function() {
-	
-	this.db.players.drop(function(error, replay) {})
+	this.find = function(player_username, callback) {
+		this.db.players.findOne(player_username, function(error, result){
+			if ( error ) callback(error)
+			callback(null, result)
+		})
+	}
+
+	this.clean = function() {
+		this.db.players.drop(function(error, replay) {})
+	}
 }
 
 exports.PlayerRepository = PlayerRepository
