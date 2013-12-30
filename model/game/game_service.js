fd8cf92e6ac227b3d8048105ab9640631bfdd944
Borja Fernández Pillado <borja.fernandez@unience.com>
2013-12-30 19:04:04 +0100

    One new find method for games and fix little error

--- model/game/game_service.js
+++ model/game/game_service.js
@@ -1,17 +1,26 @@
 GameRepository = require('../../infraestructure/repository').Repository
-Player = require('./player').Player
+Game = require('./game').Game
 
 function GameService(){
 	this.gameRepository = new GameRepository()
 
 	this.saveAGame = function(game_data, callback){
-		var game = new Game(player_data)
+		var game = new Game(game_data)
 
 		game.save(function (error, game_saved){
 			if ( error ) callback(error)
 
 			callback(null, game_saved)
 		})
+	}
+
+	this.findAGame = function(game_data, callback){
+		this.gameRepository.findGameByNameAndOwner(game_data, function (error, game_found){
+			if ( error ) callback(error)
+
+			callback(null, game_found)
+		})
+
 	}
 }
 
