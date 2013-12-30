750ee907e07684815933b74c0755faab7cb38c7d
Borja Fernández Pillado <borja.fernandez@unience.com>
2013-12-30 19:06:07 +0100

    Adding tests for games

--- test/game-test.js
+++ test/game-test.js
@@ -1,14 +1,45 @@
+var GameService = require('../model/game/game_service').GameService
+
 process.env.NODE_ENV = 'test'
 
 describe('Game Service', function(){
 
+		var gameService = new GameService()
+
+		beforeEach(function(done){
+			gameService.gameRepository.cleanGames()
+			done()
+		})
+
+		after(function(done){
+			gameService.gameRepository.cleanGames()
+			done()
+		})
+
 		describe('save a game', function(){
 			it('should save the game data in mongo', function(done){
 				var game_data = {
-					name = "Bingo"
+					name: "I will be iron man",
+					owner: "Tony Stark"
 				}
+				gameService.saveAGame(game_data, function(error, game_saved){
+					if (error) console.log(error)
+					if(game_saved.name == 'I will be iron man' && game_saved.owner == 'Tony Stark') done()
 
-				done()
+				})
+			})
+
+			it('should get the player data in mongo', function(done){
+				var game_data = {
+					name: "I will be iron man",
+					owner: "Tony Stark"
+				}
+				gameService.saveAGame(game_data, function(error, game_saved){
+					gameService.findAGame(game_data, function(error, game_found){
+						if (error) console.log(error)
+						if(game_saved.name == 'I will be iron man' && game_saved.owner == 'Tony Stark') done()
+					})
+				})
 			})
 		})
 })
