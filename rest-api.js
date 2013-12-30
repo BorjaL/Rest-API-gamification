647ae35cd06994e3a13e30b1cad5e98643008af2
Borja Fernández Pillado <borja.fernandez@unience.com>
2013-12-30 19:07:17 +0100

    Deleting comments

--- rest-api.js
+++ rest-api.js
@@ -2,29 +2,10 @@ var server = require('./infraestructure/server')
 
 server.startServer()
 
-/*server.get('/player/:id', function (req, res, next) {
-	console.log('Someone asks for %s', req.params.id)
-	res.send()
-})
-
-server.put('/player/:id', function (req, res, next) {
-	console.log('Someone wants to update to %s', req.params.username)
-	res.send()
-})
-
+/*
 server.del('/player/:id', function (req, res, next) {
 	console.log('Someone wants to delete %s', req.params.id)
 	res.send()
-})
-
-server.get('/game/:id', function (req, res, next) {
-	console.log('Someone asks for the game %s', req.params.id)
-	res.send()
-})
-
-server.post('/game', function (req, res, next) {
-	console.log('Someone wants to create a new game called %s', req.params.name)
-	res.send(201)
 })
 
 server.put('/game/:id', function (req, res, next) {
@@ -45,4 +26,5 @@ server.get('/play/:id', function (req, res, next) {
 server.post('/play', function (req, res, next) {
 	console.log('Someone wants to create a new play called %s', req.params.name)
 	res.send(201)
-})*/
+})
+*/
