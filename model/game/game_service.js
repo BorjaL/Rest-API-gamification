a13a9784ae546115ea73a7cf54feaf295f2021f9
Borja Fernandez Pillado <borja.fpillado@gmail.com>
2014-11-11 00:22:08 +0100

    Adding good practices returns before callback

--- model/game/game_service.js
+++ model/game/game_service.js
@@ -4,24 +4,28 @@ Game = require('./game').Game;
 module.exports.form_fields = function(callback){
 	var game = new Game({name: ""});
 
-	callback(null, game.defaultAttributes());
+	return callback(null, game.defaultAttributes());
 };
 
 module.exports.saveAGame = function(game_data, callback){
 	var game = new Game(game_data);
 
 	game.save(function (error, game_saved){
-		if ( error ){ callback(error); }
+		if ( error ){ 
+			return callback(error); 
+		}
 
-		callback(null, game_saved);
+		return callback(null, game_saved);
 	});
 };
 
 module.exports.findAGame = function(game_name, callback){
 	game_repository.findGameByName(game_name, function (error, game_found){
-		if ( error ){ callback(error); }
+		if ( error ){ 
+			return callback(error); 
+		}
 
-		callback(null, game_found);
+		return callback(null, game_found);
 	});
 };
 
@@ -29,18 +33,22 @@ module.exports.saveAnAction = function(action_data, callback){
 	game_repository.findGameById({_id: action_data.game}, function (error, game_found){
 		var game = new Game(game_found);
 		game.addAnAction(action_data, function(error){
-			if (error){ callback(error); }
+			if (error){ 
+				return callback(error); 
+			}
 
-			callback(null);
+			return callback(null);
 		});
 	});
 };
 
 module.exports.findAllActionsOfAGame = function(game_id, callback){
 	game_repository.findAllActionsOfAGame(game_id, function (error, actions_found){
-		if (error){ callback(error); }
+		if (error){ 
+			return callback(error); 
+		}
 
-		callback(null, actions_found);
+		return callback(null, actions_found);
 	});
 };
 
@@ -52,9 +60,11 @@ module.exports.joinToTheGame = function(userAndGameInfo, callback){
 			var player = new Player(player_found);
 			game.addAPlayer(player._id, function(error){
 				player.joinToAGame(game._id, function(error){
-					if (error){ callback(error); }
+					if (error){ 
+						return callback(error); 
+					}
 
-					callback(null);
+					return callback(null);
 				});
 			});
 		});
--- model/player/player_service.js
+++ model/player/player_service.js
@@ -5,40 +5,52 @@ var player_factory = require('./player_factory');
 module.exports.form_fields = function(callback){
 	var player = player_factory.getPlayerObjectWith({repository: player_repository});
 
-	callback(null, player.defaultAttributes());
+	return callback(null, player.defaultAttributes());
 };
 
 module.exports.saveAPlayer = function(player_data, callback){
 	var player = player_factory.getPlayerObjectWith(player_data + {repository: player_repository});
 
 	player.save(function (error, player_saved){
-		if ( error ){ callback(error); }
+		if ( error ){ 
+			return callback(error); 
+		}
 
-		callback(null, player_saved);
+		return callback(null, player_saved);
 	});
 };
 
 module.exports.findAPlayer = function(player_username, callback){
 	player_repository.findPlayerByUsername(player_username, function (error, player_found){
-		if ( error ){ callback(error); }
+		if ( error ){ 
+			return callback(error); 
+		}
 
-		callback(null, player_found);
+		return callback(null, player_found);
 	});
 };
 
 module.exports.logIn = function(username, password, callback){
 	player_repository.findPlayerByUsername(username, function (error, player_found_data){
-		if ( error ){ callback(error); }
-		if (player_found_data == null){ callback(false); }
+		if ( error ){ 
+			return callback(error); 
+		}
+		if (player_found_data == null){ 
+			return callback(false); 
+		}
 
 		var player_found = player_factory.getPlayerObjectWith(player_found_data + {repository: player_repository});
 
 		player_found.verifyPassword(password, function(error, isMatch){
-			if (error){ callback(error); }
+			if (error){ 
+				return callback(error); 
+			}
 			if (isMatch){
-				callback(null, player_found.username);
+				return callback(null, player_found.username);
 			} 
-			else{ callback(null, false); }
+			else{ 
+				return callback(null, false); 
+			}
 		});
 		
 	});
