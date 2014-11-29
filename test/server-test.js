e922744a14b27783dddfcea20582bdf06ce8187b
Borja Fernández Pillado <borja.fpillado@gmail.com>
2014-11-29 14:51:34 +0100

    Adding my first server test

---
+++ test/server-test.js
@@ -0,0 +1,35 @@
+var assert = require('assert');
+var sinon = require('sinon');
+var client = require('restify').createJsonClient({
+    version: '*',
+    url: 'http://dev.gamification.com:3001'
+});
+var game_service = require('../model/game/game_service');
+
+process.env.NODE_ENV = 'test';
+require('../infraestructure/server').startServer();
+
+describe('Server', function(){
+	it('save a new game', function(done){
+
+		//given:
+        var data = {game: true};
+		var saveAStub = sinon.stub(game_service, "saveAGame").callsArgWith(1, null, data);
+
+		//when:
+		client.post('/games.json', {},function(err, req, res, data) {
+            if (err) {
+                throw new Error(err);
+            }
+            else {
+                if (!data.game) {
+                    throw new Error('invalid response from /games.json');
+                }
+
+                assert.equal(res.statusCode, 201);
+                assert.equal(res.body, JSON.stringify(data));
+                done();
+            }
+        });
+	});
+});
